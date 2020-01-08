314ac55df30b034ea2d623ee89a65019f8aa551c
Aden Herold <aden.herold1@gmail.com>
2020-01-08 21:49:07 +1100

    Added @material-ui/core dependency to frontend app and added basic theme

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./pages/App";
-import * as serviceWorker from "./serviceWorker";
 import { ApolloProvider } from "react-apollo";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import theme from "./theme";
+import App from "./pages/App";
 import client from "./apolloClient";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </ThemeProvider>,
   document.getElementById("root")
 );
 
--- frontend/src/pages/App/App.tsx
+++ frontend/src/pages/App/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Chat from "../Chat";
 
-const App: React.FC = () => {
+const App: React.SFC = () => {
   return (
     <div className="App">
       <h1>Tangent</h1>
